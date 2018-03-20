532363bb3dd3aa4a3262d872a820f87b9dd6be77
Guo Du <mrduguo@gmail.com>
2018-03-20 16:04:27 +0000

    moved App to app package

--- src/main/java/com/github/mrduguo/spring/app/App.java
+++ src/main/java/com/github/mrduguo/spring/app/App.java
@@ -1,3 +1,5 @@
+package com.github.mrduguo.spring.app;
+
 import com.github.mrduguo.spring.app.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
